c447313a8ca99da1d9e9ecd6cd0366b6ba912876
pedrogplopes <pl.pedrogplopes@gmail.com>
2024-08-31 22:54:04 -0300

    Changes to superuser

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\pedro.lopes\Downloads\APS2\venv
+set VIRTUAL_ENV=C:\Users\inter_bg3ur0g\Downloads\Trabalho APS2\APS2_Trabalho\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
